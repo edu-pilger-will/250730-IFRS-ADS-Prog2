a3d9a8477bb51a54a1764226b34ea10a20f0be47
edu-pilger-will <129803121+edu-pilger-will@users.noreply.github.com>
2025-07-30 20:55:51 -0300

    clean

---
+++ aula01/src/exemplos/Leitura.java
@@ -0,0 +1,16 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class Leitura {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Digita um número: ");
+		int n = Integer.parseInt(leitor.nextLine());
+		
+		System.out.println("Usuário digitou " + n);
+	}
+
+}
